f50bb0a489be8e35c0ff9a9ec8a623042e73bbe6
Damien Sedgwick <damienksedgwick@gmail.com>
2025-06-27 20:06:26 +0100

    Initial Commit

---
+++ Checkpoint/CheckpointApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckpointApp.swift
+//  Checkpoint
+//
+//  Created by Damien Sedgwick on 27/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct CheckpointApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
