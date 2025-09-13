d05b3c74c2368dc58ad1c7fbb05799c34dde7ab1
Damien Sedgwick <damienksedgwick@gmail.com>
2025-09-13 20:55:40 +0100

    created a brand new project using xcode 26 and ready for some glass 🥃

---
+++ Checkpoint/checkpointApp.swift
@@ -0,0 +1,17 @@
+//
+//  checkpointApp.swift
+//  checkpoint
+//
+//  Created by Damien Sedgwick on 13/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct checkpointApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ checkpointTests/checkpointTests.swift
@@ -0,0 +1,17 @@
+//
+//  checkpointTests.swift
+//  checkpointTests
+//
+//  Created by Damien Sedgwick on 13/09/2025.
+//
+
+import Testing
+@testable import checkpoint
+
+struct checkpointTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
